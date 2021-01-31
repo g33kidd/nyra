6854ed82bdefb00f728af90c9031ab98b4bab7bf
Joshua Kidd <kidd.josh.343@gmail.com>
2021-01-31 00:20:02 -0600

    Added authentication code generator in the Bouncer.

--- lib/nyra/bouncer.ex
+++ lib/nyra/bouncer.ex
@@ -1,62 +1,49 @@
 defmodule Nyra.Bouncer do
   use GenServer
 
+  @moduledoc """
+  [Bouncer] is the coolest dude around.
+  It will generate a randomly generated code for the user to auth with.
+  """
+
   @name __MODULE__
   @expires 120
 
   def start_link(_), do: GenServer.start_link(__MODULE__, [], name: @name)
 
-  def init(_) do
+  @doc """
+  Guestlists a user in the Bouncer application. They will be tested later when they try to enter :)
+  """
+  def guestlist(sid) do
+    GenServer.call(@name, {:guestlist, sid})
+  end
+
+  def is_cool?(pid, code) do
+    GenServer.call(@name, {:check, pid, code})
+  end
+
+  # Callbacks
+
+  @impl true
+  def init(state = []) do
+    # Removes codes that have been used or not used within the expiration time.
     :timer.send_interval(@expires * 120_000, :expire_codes)
-    :ets.new(@name, [:set, :public, :named_table])
-    IO.puts("[info] ETS table for #{@name} was created")
-    {:ok, :bouncer_created}
+
+    # initializing the state
+    {:ok, state}
   end
 
-  # This next bit of code should do the following:
-  # 1. generate a random 4 digit code.
-  # 2. associate code w/ id
-  # 3. store in ets with expiry..
-  def handle_call({:assign, socket_id}, _, state) do
-    code = Enum.random(1_000..9_999)
-    IO.inspect(code)
-    expiration = :os.system_time(:seconds) + @expires
-    :ets.insert(@name, {socket_id, code, expiration})
-
-    {:reply, :ok, state}
+  def handle_cast({:guestlist, sid}, state) do
+    {:noreply, [{generate_code(sid), sid} | state]}
   end
 
-  def handle_call(:expire_codes, _, state) do
-    IO.inspect(state)
-    {:noreply, state}
-  end
-
-  def handle_call({:verify, socket_id, input}, _, _) do
-    case :ets.lookup(@name, socket_id) do
-      [result | rest] ->
-        IO.inspect(result)
-        IO.inspect(rest)
-        {:reply, :ok, validate_coolness(result, input)}
-
-      [] ->
-        {:reply, :error, "Invalid code."}
-    end
-  end
-
-  def expired?(exp), do: exp > :os.system_time(:seconds)
-
-  def validate_coolness({_id, code, expiration}, input) do
-    case expired?(expiration) do
-      false -> {:ok, String.equivalent?(code, input)}
-      true -> :expired
-    end
-  end
-
-  def assign_code(socket_id) do
-    GenServer.call(@name, {:assign, socket_id})
-  end
-
-  def check(socket_id, code) do
-    GenServer.call(@name, {:verify, socket_id, code})
+  def generate_code(salt \\ "crypto") do
+    :crypto.hash(:sha256, salt)
+    |> Base.encode64()
+    |> String.to_charlist()
+    |> Enum.shuffle()
+    |> Enum.take(6)
+    |> :binary.list_to_bin()
+    |> String.upcase()
   end
 end
--- lib/nyra_web/live/page_live.ex
+++ lib/nyra_web/live/page_live.ex
@@ -1,6 +1,7 @@
 defmodule NyraWeb.PageLive do
   use NyraWeb, :live_view
 
+  alias Nyra.Bouncer
   alias Nyra.Accounts
   alias Nyra.Accounts.User
 
@@ -32,20 +33,8 @@ defmodule NyraWeb.PageLive do
 
   @impl true
   def handle_event("verify", %{"code" => code}, socket) do
-    Nyra.Bouncer.check(socket.id, code)
-    |> IO.inspect()
 
-    with :ok <- Nyra.Bouncer.check(socket.id, code) do
-      socket =
-        socket
-        |> put_flash(:info, "You're in! Congrats..")
-
-      {:noreply, socket}
-    else
-      nil -> {:noreply, assign(socket, :error_message, "Invalid code.")}
-      {:error, ugh} -> {:noreply, assign(socket, :error_message, ugh)}
-      :else -> {:noreply, assign(socket, :error_message, "Not sure this time..")}
-    end
+    {:noreply, socket}
   end
 
   defp authenticate(socket, email) do
@@ -58,26 +47,17 @@ defmodule NyraWeb.PageLive do
       {:ok, user} -> handle_with_user(socket, user)
       {:error, changeset} -> {:error, changeset}
     end
-
-    # case Accounts.get_user_by(email: email) do
-    #   {:ok, nil} ->
-    #     handle_no_user(assign(socket, :changeset, changeset))
-
-    #   {:ok, user} ->
-    #     handle_with_user(assign(socket, :changeset, changeset), user)
-    # end
   end
 
   # Modifies and returns a new socket with information regarding an existing user.
   defp handle_with_user(socket, user) do
     Nyra.Bouncer.assign_code(socket.id)
-    |> IO.inspect()
 
     socket
+    |> put_flash(:welcome_back, @welcome_back)
     |> assign(new_user: false)
     |> assign(current_user: user)
     |> assign(awaiting_code: true)
-    |> put_flash(:welcome_back, @welcome_back)
 
     # it works, just make sure the right messages are sent..
   end
@@ -87,48 +67,9 @@ defmodule NyraWeb.PageLive do
     # it works, just make sure the right messages are sent..
 
     socket
+    |> put_flash(:welcome, @welcome_msg)
     |> assign(new_user: true)
     |> assign(awaiting_code: true)
     |> assign(current_user: user)
-    |> put_flash(:welcome, @welcome_msg)
   end
-
-  # @impl true
-  # def handle_info("verify_user:" <> user, socket) do
-  #   Phoenix.PubSub.broadcast(Nyra.PubSub, "verify_user:" <> user, {})
-
-  #   {:noreply, socket}
-  # end
-
-  # @impl true
-  # def handle_event("authenticate", %{"email" => _email}, socket) do
-  #   socket = socket |> assign(waiting_verification: true)
-  #   {:noreply, socket}
-  # end
-
-  # @impl true
-  # def handle_event("search", %{"q" => query}, socket) do
-  #   case search(query) do
-  #     %{^query => vsn} ->
-  #       {:noreply, redirect(socket, external: "https://hexdocs.pm/#{query}/#{vsn}")}
-
-  #     _ ->
-  #       {:noreply,
-  #        socket
-  #        |> put_flash(:error, "No dependencies found matching \"#{query}\"")
-  #        |> assign(results: %{}, query: query)}
-  #   end
-  # end
-
-  # defp search(query) do
-  #   if not NyraWeb.Endpoint.config(:code_reloader) do
-  #     raise "action disabled when not in development"
-  #   end
-
-  #   for {app, desc, vsn} <- Application.started_applications(),
-  #       app = to_string(app),
-  #       String.starts_with?(app, query) and not List.starts_with?(desc, ~c"ERTS"),
-  #       into: %{},
-  #       do: {app, vsn}
-  # end
 end
